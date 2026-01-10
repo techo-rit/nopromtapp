5b421355b35824b44cdc766a02172c606262f85c
techo-rit <techorit12345@gmail.com>
2026-01-10 05:11:09 +0000

    fix: Correct CSS import path in index.tsx

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
-import './src/index.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
