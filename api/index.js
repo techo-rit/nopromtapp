fdeb9ba802016f10949046be65c2b8d94df29d7f
Rishabh Thakur <rishabh12in2019@gmail.com>
2026-02-09 20:49:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,9 @@ let handler;
 
 module.exports = async (req, res) => {
   if (!handler) {
-    const [{ default: serverless }, { createApp }] = await Promise.all([
-      import('serverless-http'),
-      import('../server/src/app.js'),
-    ]);
-    handler = serverless(createApp());
+    const { createApp } = await import('../server/src/app.js');
+    const app = createApp();
+    handler = (req, res) => app(req, res);
   }
   return handler(req, res);
 };
