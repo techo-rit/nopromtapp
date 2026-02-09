ba70ebb9df4e78477b0ee5f865aa7e469692db66
Rishabh Thakur <rishabh12in2019@gmail.com>
2026-02-09 20:44:51 +0530

    Update Deployment Script

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
-import serverless from 'serverless-http';
-import { createApp } from '../server/src/app.js';
+let handler;
 
-const app = createApp();
-
-export default serverless(app);
+module.exports = async (req, res) => {
+  if (!handler) {
+    const [{ default: serverless }, { createApp }] = await Promise.all([
+      import('serverless-http'),
+      import('../server/src/app.js'),
+    ]);
+    handler = serverless(createApp());
+  }
+  return handler(req, res);
+};
